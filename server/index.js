8c4345babf8bf82b803e5dbf6c978953c21a7de4
Sujan S <sujanshivananda5@gmail.com>
2026-03-23 19:29:23 +0530

    fix: render

--- server/index.js
+++ server/index.js
@@ -30,10 +30,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
